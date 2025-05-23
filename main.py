c76e7bbf2623d5ce7839d794afcbe643eb60d8e5
Laszlo Roovers <l.roovers@student.tudelft.nl>
2025-05-23 12:22:18 +0200

    Implemented spatial downsampling

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 from data_processing.amplitudinal import amplitudinal_downsample
+from data_processing.spatial import spatial_downsample
 
 amplitudinal_downsample("./data/example.jpg", 51)
+spatial_downsample("./data/example.jpg", 0.5)
