1f7574253b09e27167408bb0e61836ab6594cb57
Laszlo Roovers <l.roovers@student.tudelft.nl>
2025-05-23 12:11:41 +0200

    Finalize amplitudinal downsampling implementation

--- data_processing/amplitudinal.py
+++ data_processing/amplitudinal.py
@@ -2,7 +2,8 @@ import os
 import subprocess
 from PIL import Image
 
-def amplitudinal_downsample(input_path : str, qp : int, output_path : str) -> Image.Image:
+
+def amplitudinal_downsample(input_path: str, qp: int) -> Image.Image:
     """
     Compresses and decompresses an image using BPG to reduce amplitude resolution.
     Essentially, this function is a python wrapper around the BPG encoder and decoder.
@@ -10,20 +11,23 @@ def amplitudinal_downsample(input_path : str, qp : int, output_path : str) -> Im
     Args:
         input_path (str): Path to the input image.
         qp (int): Quantization parameter for BPG compression (0-51).
-        output_path (str): Path to save the decompressed image.
-    
+
     Returns:
         Image.Image: The decompressed image.
     """
     # Set intermediate and output paths
-    bpg_path = input_path.replace('.png', f'_qp{qp}.bpg').replace('.jpg', f'_qp{qp}.bpg')
-    output_path = input_path.replace(".png", f"_qp{qp}_out.png").replace(".jpg", f"_qp{qp}_out.png")
+    bpg_path = input_path.replace(".png", f"_qp{qp}.bpg").replace(
+        ".jpg", f"_qp{qp}.bpg"
+    )
+    output_path = input_path.replace(".png", f"_qp{qp}_out.png").replace(
+        ".jpg", f"_qp{qp}_out.png"
+    )
 
     # Perform BPG compression
-    subprocess.run(['bpgenc', '-q', str(qp), '-o', bpg_path, input_path], check=True)
+    subprocess.run(["bpgenc", "-q", str(qp), "-o", bpg_path, input_path], check=True)
 
     # Perform BPG decompression
-    subprocess.run(['bpgdec', '-o', output_path, bpg_path], check=True)
+    subprocess.run(["bpgdec", "-o", output_path, bpg_path], check=True)
 
     # Clean up the intermediate BPG file
     os.remove(bpg_path)
---
+++ main.py
@@ -0,0 +1,3 @@
+from data_processing.amplitudinal import amplitudinal_downsample
+
+amplitudinal_downsample("./data/example.jpg", 51)
