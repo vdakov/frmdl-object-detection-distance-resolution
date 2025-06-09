00919f4c2d4a453774c108ddf5092687fc3c7d70
vdakov <v.dakov02@gmail.com>
2025-06-09 18:21:38 +0200

    Add spatial downsampling for dict elements  in function + rounding

--- data_processing/spatial.py
+++ data_processing/spatial.py
@@ -1,7 +1,8 @@
+from typing import List
 from PIL import Image
+import json 
 
-
-def spatial_downsample(input_path: str, scale_factor: float) -> Image.Image:
+def spatial_downsample(input_path: str, label_path:str, label_values_to_scale:List[str], scale_factor: float) -> Image.Image:
     """
     Downsamples the image resolution by a scale factor.
 
@@ -18,12 +19,24 @@ def spatial_downsample(input_path: str, scale_factor: float) -> Image.Image:
 
     # Open image
     img = Image.open(input_path)
+    with open(label_path, 'r') as f:
+        label_dict = json.load(f)
+    
 
     # Compute new size
     new_width = int(img.width * scale_factor)
     new_height = int(img.height * scale_factor)
+    for label_key in label_values_to_scale:
+        if label_key in label_dict.keys():
+            label_dict[label_key] *= scale_factor
+            label_dict[label_key] = int(label_dict[label_key])
+        elif "children" in label_dict.keys():
+            for child in label_dict["children"]:
+                if label_key in child.keys():
+                    child[label_key] *= scale_factor
+                    child[label_key] = int(child[label_key])  
 
     # Downsample using high-quality resampling
     downsampled_img = img.resize((new_width, new_height), Image.Resampling.BILINEAR)
 
-    return downsampled_img
+    return downsampled_img, label_dict
