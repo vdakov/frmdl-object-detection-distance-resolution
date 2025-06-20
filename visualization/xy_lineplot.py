33b079fca45fffad11c9a946853150ce2e7a981f
vdakov <v.dakov02@gmail.com>
2025-06-20 20:19:27 +0200

    Visualization plot

--- visualization/xy_lineplot.py
+++ visualization/xy_lineplot.py
@@ -10,11 +10,16 @@ def basic_lineplot(x, y, title="Line Plot", xlabel="X-axis", ylabel="Y-axis"):
     plt.show()
     
 
+
 def multi_lineplot(data_dict, title="Line Plot", xlabel="X-axis", ylabel="Y-axis"):
     plt.figure(figsize=(8, 5))
+    plt.title(title)
 
-    for label, (x, y) in data_dict.items():
+    for label, (x, y, texts) in data_dict.items():
         plt.plot(x, y, marker='o', linestyle='-', label=label)
+        for xi, yi, txt in zip(x, y, texts):
+            plt.text(xi, yi, str(txt), fontsize=9, ha='right', va='bottom')
+
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     plt.grid(True)
